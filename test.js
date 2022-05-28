3f459200dba192027a6c88cbcb6ec6aaf735fded
Kyle E. Mitchell <kyle@kemitchell.com>
2022-05-28 11:18:44 -0700

    Improve version regular expression

--- test.js
+++ test.js
@@ -3,6 +3,44 @@ const addFormats = require('ajv-formats')
 const schema = require('./')
 const tape = require('tape')
 
+// Versions
+
+const versionRE = new RegExp(schema.properties.version.pattern)
+
+const versions = {
+  valid: [
+    '1.2.3',
+    '10.20.30',
+    '100.200.300',
+    '1.2.3-4',
+    '1.2.3-45'
+  ],
+  invalid: [
+    '001.2.3',
+    '01.2.3',
+    '1.02.3',
+    '1.2.03',
+    '1.0.0-0',
+    '1.0.0-01'
+  ]
+}
+
+tape('valid versions', test => {
+  for (const version of versions.valid) {
+    test.assert(versionRE.test(version), `${version} valid`)
+  }
+  test.end()
+})
+
+tape('invalid versions', test => {
+  for (const version of versions.invalid) {
+    test.assert(!versionRE.test(version), `${version} invalid`)
+  }
+  test.end()
+})
+
+// Full Objects
+
 const ajv = new Ajv({ allErrors: true })
 addFormats(ajv)
 const validate = ajv.compile(schema)
@@ -23,14 +61,6 @@ const valid = {
   }
 }
 
-for (const label in valid) {
-  tape(`valid: ${label}`, test => {
-    validate(valid[label])
-    test.deepEqual(validate.errors, null, label)
-    test.end()
-  })
-}
-
 const invalid = {
   'no publisher': Object.assign({}, valid['California Governing Law'], { publisher: undefined }),
   'malformed version': {
@@ -45,6 +75,14 @@ const invalid = {
     version: '1X2X3',
     content: ['example']
   }
+}
+
+for (const label in valid) {
+  tape(`valid: ${label}`, test => {
+    validate(valid[label])
+    test.deepEqual(validate.errors, null, label)
+    test.end()
+  })
 }
 
 for (const label in invalid) {
